3cb941471ba12094c1be694d67eb36feb116b7b2
Steve <steve.gitprofile@outlook.com>
2024-05-13 14:21:03 +0200

    get current state with callback

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,43 +7,67 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 2;
+  // want step to be dynamic
+  //  need piece of state
+  // const step = 2;
+
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    alert("Previous");
+    if (step === 1) {
+      return;
+    }
+    // if set state based on current state use call back to retrieve current state
+    setStep((s) => s - 1);
   }
 
   function handleNext() {
-    alert("Next");
+    if (step === 3) {
+      return;
+    }
+    // callback to get current state
+    setStep((s) => s + 1);
   }
 
+  //  cant just return { } with a conditional render for isOpen
+  // needs to be inside a component - div or fragement to start JSX (NB to remember)
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
+    <>
+      {/* dont always have to write function first can use annonymouse */}
+      <button className="close" onClick={() => setIsOpen((isO) => !isO)}>
+        {isOpen ? "×" : "⊕"}
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            {step > 0 && `Step ${step}: ${messages[step - 1]}`}
+          </p>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          // can listen to more than just click events
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              // can listen to more than just click events
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
